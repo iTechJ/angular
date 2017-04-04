f27743ee7a66be623eae0a713a630ebfe239cb21
anton-nekrasov-itechartgroup <anton.nekrasov@itechart-group.com>
2017-04-04 14:52:49 +0300

    main pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from "@angular/router";
+import { HashLocationStrategy, LocationStrategy, APP_BASE_HREF } from "@angular/common";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ListComponent } from './components/book/list/list.component';
+import { DetailsComponent } from './components/book/details/details.component';
+
+const routes: Routes = [
+  {
+    "path": "",
+    "redirectTo": "home",
+    "pathMatch": "full"
+  }, {
+    "path": "home",
+    "component": HomeComponent,
+  }, {
+    "path": "book/list",
+    "component": ListComponent,
+  }, {
+    "path": "book/details/:id",
+    "component": DetailsComponent,
+  }, {
+    "path": "about",
+    "component": AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    {"provide": APP_BASE_HREF, "useValue": "/"},
+    {"provide": LocationStrategy, "useClass": HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/book/book.ts
@@ -0,0 +1,11 @@
+export class Book {
+  constructor(
+    public sku: string,
+    public name: string,
+    public author: string,
+    public description: string,
+    public price: number
+  ) {
+
+  }
+}
