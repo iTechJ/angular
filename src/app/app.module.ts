5aa14913f68139eda13469025cde64cb716d47b6
anton.nekrasov <anton.nekrasov@itechart-group.com>
2017-04-26 22:29:04 +0300

    sub routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,16 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from "@angular/router";
 import { HashLocationStrategy, LocationStrategy, APP_BASE_HREF } from "@angular/common";
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { ListComponent } from './components/book/list/list.component';
-import { DetailsComponent } from './components/book/details/details.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { BookComponent } from './components/book/book/book.component';
 import { CartFormComponent } from './components/shopping-cart/cart-form/cart-form.component';
-import {bookServiceInjectables} from "./components/book/book-service-injectables";
+import { bookServiceInjectables } from "./components/book/book-service-injectables";
+import { BookModule, routes as bookRoutes } from "./components/book/book/book.module";
+import { BookContainerComponent } from './components/book/book-container/book-container.component';
 
 const routes: Routes = [
   {
@@ -26,12 +24,11 @@ const routes: Routes = [
     "path": "home",
     "component": HomeComponent
   }, {
-    "path": "book/list",
-    "component": ListComponent
-  }, {
-    "path": "book/details/:id",
-    "component": DetailsComponent
-  }, {
+    "path": "book",
+    "component": BookContainerComponent,
+    "children": bookRoutes
+  },
+  {
     "path": "cart",
     "component": ShoppingCartComponent
   }, {
@@ -45,20 +42,19 @@ const routes: Routes = [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ListComponent,
-    DetailsComponent,
     HeaderComponent,
     FooterComponent,
     ShoppingCartComponent,
-    BookComponent,
-    CartFormComponent
+    CartFormComponent,
+    BookContainerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BookModule
   ],
   providers: [
     {"provide": APP_BASE_HREF, "useValue": "/"},
